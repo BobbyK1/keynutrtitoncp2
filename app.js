fec9a96daecf91b257024a5c404a135fd12b2dda
BobbyK1 <marbobkara@gmail.com>
2018-10-29 04:16:48 -0500

    UI and Backend Changes

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = ("body-parser");
+const mongoose = require("mongoose");
+const favicon = require("serve-favicon");
+
+mongoose.connect("mongodb://admin:OnlyForContact1@ds145043.mlab.com:45043/keynutrition-contact", { useNewUrlParser: true });
 
 app.use(express.static("public"));
+app.use(favicon(__dirname + '/public/images/herbalife.png'));
 app.set("view engine", "ejs");
+
+// CONTACT SCHEMA
+var contactSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    message: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Contact = mongoose.model("Contact", contactSchema);
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -17,6 +32,6 @@ app.get("/contact-us", function(req, res) {
     res.render("contact-us");
 })
 
-app.listen(process.env.PORT, function() {
+app.listen(8080, function() {
     console.log("Server Started on Port: 8080");
 });
